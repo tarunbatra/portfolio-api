85ed65dab7185143ebcf5c2e97a3530fbf507b61
Tarun <tarun.batra00@gmail.com>
2017-07-24 00:03:43 +0530

    Adds mongoose, body parser and other packages

--- index.js
+++ index.js
@@ -3,13 +3,42 @@
  */
 
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Responder = require('./services/responder');
+const Trades = require('./controllers/trades');
 
 const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/portfolio';
 
 const app = express();
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
+// initialize response payload
+app.use(Responder.init);
+
+app.get('/trades/:id', Trades.get);
+app.get('/', Trades.fetchPortfolio);
+app.post('/addTrade', Trades.create);
+app.post('/updateTrade/:id', Trades.update);
+app.post('/removeTrade/:id', Trades.delete);
+
+// Dispatch response payload
+app.use(Responder.reply);
 
 // Start the server
-const server = app.listen(PORT, (err) => {
-  err ? console.error(err)
-      : console.log(`Server running at ${PORT}`);
-});
+connectDatabase()
+  .on('error', console.log)
+  .on('disconnected', connectDatabase)
+  .once('open', listenRequests);
+
+function listenRequests () {
+  app.listen(PORT);
+  console.log(`Server running at ${PORT}`);
+}
+
+function connectDatabase () {
+  return mongoose.connect(MONGO_URL).connection;
+}

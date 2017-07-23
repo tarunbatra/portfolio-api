acdaf4b02659051c97553fe10f7c4b2da07aaff5
Tarun <tarun.batra00@gmail.com>
2017-07-23 23:58:35 +0530

    Adds trades controllers

---
+++ controllers/trades.js
@@ -0,0 +1,58 @@
+/**
+ * @file Trades Controller
+ */
+
+
+const mongoose = require('mongoose');
+const _ = require('underscore');
+const Stocks = require('../services/stocks');
+const Trade = require('../models/trade');
+
+const findOptions = '-__v';
+
+module.exports = {
+
+  /**
+   * Creates a new trade
+   */
+  create: (req, res, next) => {
+    req.obj.price = Stocks.fetchPrice(req.obj.stock);
+    const trade = new Trade(req.obj);
+    req.data.trade = trade;
+    trade.save(next);
+  },
+
+  /**
+   * Return a trade by it's id
+   */
+  get: (req, res, next) => {
+    Trade.findById(req.params.id, findOptions, (err, data) => {
+      req.data.trade = data;
+      next(err);
+    });
+  },
+
+  /**
+   * Return the Trades grouped by stock
+   */
+  fetchPortfolio: (req, res, next) => {
+    Trade.find({}, findOptions, (err, data) => {
+      req.data.portfolio = _.groupBy(data, 'stock');
+      next(err);
+    });
+  },
+
+  /**
+   * Update a trade by it's id
+   */
+  update: (req, res, next) => {
+    Trade.update(req.params.id, req.obj, next);
+  },
+
+  /**
+   * Delete a trade by it's id
+   */
+  delete: (req, res, next) => {
+    Trade.deleteOne({ _id: req.params.id }, next);
+  }
+};

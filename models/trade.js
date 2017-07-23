3e4a851357f69e6e6f1cd1fe32f170de8148dc29
Tarun <tarun.batra00@gmail.com>
2017-07-23 23:58:24 +0530

    Adds trades model

---
+++ models/trade.js
@@ -0,0 +1,21 @@
+/**
+ * @file Trades of a stock
+ */
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/**
+ * Trades Schema
+ */
+
+const TradeSchema = new Schema({
+  stock: { type: String, required: true },
+  type: { type: String, enum: [ 'BUY', 'SELL' ], required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Trade', TradeSchema);
